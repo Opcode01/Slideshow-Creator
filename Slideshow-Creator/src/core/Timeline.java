073a1f2cc0c36481d9de1036cc2dc5c2941e890a
TJ-Desktop <tjcouch1@gmail.com>
2019-02-13 21:32:58 -0600

    Added thumbnails and transitions to Timeline, created default constructor

--- Slideshow-Creator/src/core/Timeline.java
+++ Slideshow-Creator/src/core/Timeline.java
@@ -1,5 +1,28 @@
+/**
+ * Timeline.java
+ * Coordinates thumbnails and transitions
+ * 
+ * Slideshow Creator
+ * Timothy Couch, Joseph Hoang, Fernando Palacios, Austin Vickers
+ * CS 499 Senior Design with Dr. Rick Coleman
+ * 2/13/19
+ */
+
 package core;
 
-public class Timeline {
+public class Timeline
+{
+    public Thumbnails thumbnails;
+    public Transitions transitions;
 
+    /**
+     * Timeline - initializes an empty timeline
+     * 
+     * @author Timothy Couch
+     */
+    public Timeline()
+    {
+        thumbnails = new Thumbnails();
+        transitions = new Transitions();
+    }
 }
