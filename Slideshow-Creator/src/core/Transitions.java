d277293faaa8de7512195278532949f685daacf5
TJ-Desktop <tjcouch1@gmail.com>
2019-02-13 21:32:29 -0600

    Added skeleton class for Thumbnails, Transitions

---
+++ Slideshow-Creator/src/core/Transitions.java
@@ -0,0 +1,16 @@
+/**
+ * Transitions.java
+ * Holds a list of transitions
+ * 
+ * Slideshow Creator
+ * Timothy Couch, Joseph Hoang, Fernando Palacios, Austin Vickers
+ * CS 499 Senior Design with Dr. Rick Coleman
+ * 2/13/19
+ */
+
+package core;
+
+public class Transitions
+{
+
+}
